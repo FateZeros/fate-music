209fbcb0fb44e32d4f462b55a4c431214367aed2
FateZeros <490293266@qq.com>
2020-11-29 20:41:25 +0800

    update

--- electron/electron.js
+++ electron/electron.js
@@ -3,14 +3,15 @@ const { app, BrowserWindow } = require('electron')
 const path = require('path')
 // eslint-disable-next-line
 const url = require('url')
-
+console.log(process.env.REACT_APP_NODE_ENV)
+// 是否开发环境
+const isDev = process.env.REACT_APP_NODE_ENV === 'development'
 let win
 
 function createWindow() {
   // 创建浏览器窗口
   win = new BrowserWindow({
-    width: 1000,
-    // width: 1550,
+    width: isDev ? 1550 : 1000,
     height: 670,
     resizable: false,
     // titleBarStyle: 'hidden',
@@ -21,19 +22,21 @@ function createWindow() {
     }
   })
 
-  // 加载远程URL,用于项目开发
-  win.loadURL('http://localhost:3000/')
-  // 打开开发者工具，默认不打开
-  win.webContents.openDevTools()
-
-  // 或加载本地HTML文件，用于项目打包
-  // win.loadURL(
-  //   url.format({
-  //     pathname: path.join(__dirname, '../build/index.html'),
-  //     protocol: 'file:',
-  //     slashes: true
-  //   })
-  // )
+  if (isDev) {
+    // 加载远程URL,用于项目开发
+    win.loadURL('http://localhost:3000/')
+    // 打开开发者工具，默认不打开
+    win.webContents.openDevTools()
+  } else {
+    // 或加载本地HTML文件，用于项目打包
+    win.loadURL(
+      url.format({
+        pathname: path.join(__dirname, '../build/index.html'),
+        protocol: 'file:',
+        slashes: true
+      })
+    )
+  }
 
   // 窗口是否可以拖动
   win.setMovable(true)
--- src/views/App.tsx
+++ src/views/App.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 
 const App = () => {
-  return <BrowserRouter />
+  return (
+    <BrowserRouter>
+      {process.env.NODE_ENV}
+      {process.env.REACT_APP_NODE_ENV}
+    </BrowserRouter>
+  )
 }
 
 export default App
