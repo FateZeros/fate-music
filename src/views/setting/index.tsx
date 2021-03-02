45552d02b675955b684e4b5942be4c97ab524d23
FateZeros <490293266@qq.com>
2021-03-02 10:13:41 +0800

    update

--- src/views/setting/index.tsx
+++ src/views/setting/index.tsx
@@ -1,7 +1,31 @@
 import React from 'react'
 
+import { ReducerContext } from 'reducers'
+// import { getUserInfo } from 'utils/auth'
+
+import styles from './index.module.scss'
+
+const { useContext } = React
+
 const Setting = () => {
-  return <div>设置</div>
+  const [_, dispatch] = useContext(ReducerContext)
+
+  const handleLogin = () => {
+    dispatch({ type: 'SHOW_LOGIN' })
+  }
+
+  return (
+    <div className={styles.wrap}>
+      <div className={styles['login-row']}>
+        <div className={styles['login-tips']}>登录后，即享高品质音乐</div>
+        <div className={styles['login-row-content']}>
+          <div className={styles['login-btn']} onClick={handleLogin}>
+            登录
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Setting
