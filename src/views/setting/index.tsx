248626215ae25ad7794fc32ef8c035c0f8dfdaa2
FateZeros <490293266@qq.com>
2021-03-03 21:50:56 +0800

    update

--- src/views/setting/index.tsx
+++ src/views/setting/index.tsx
@@ -1,14 +1,25 @@
 import React from 'react'
 
 import { ReducerContext } from 'reducers'
-// import { getUserInfo } from 'utils/auth'
+import { getUserInfo } from 'utils/auth'
 
 import styles from './index.module.scss'
 
-const { useContext } = React
+const { useContext, useEffect, useState, Fragment } = React
 
 const Setting = () => {
   const [_, dispatch] = useContext(ReducerContext)
+
+  // 用户是否已登录
+  const [userLogged, setUserLogged] = useState(false)
+
+  useEffect(() => {
+    const { token } = getUserInfo() as any
+    if (token) {
+      setUserLogged(true)
+    }
+    // eslint-disable-next-line
+  }, [])
 
   const handleLogin = () => {
     dispatch({ type: 'SHOW_LOGIN' })
@@ -17,12 +28,26 @@ const Setting = () => {
   return (
     <div className={styles.wrap}>
       <div className={styles['login-row']}>
-        <div className={styles['login-tips']}>登录后，即享高品质音乐</div>
-        <div className={styles['login-row-content']}>
-          <div className={styles['login-btn']} onClick={handleLogin}>
-            登录
-          </div>
-        </div>
+        {userLogged ? (
+          <Fragment>
+            <div className={styles['login-info1']}>
+              <div>账号</div>
+            </div>
+            <div className={styles['login-info2']}>
+              <div className={styles['bind-account-btn']}>绑定账号</div>
+              <div className={styles['update-user-btn']}>修改个人信息</div>
+            </div>
+          </Fragment>
+        ) : (
+          <Fragment>
+            <div className={styles['login-tips']}>登录后，即享高品质音乐</div>
+            <div className={styles['login-row-content']}>
+              <div className={styles['login-btn']} onClick={handleLogin}>
+                登录
+              </div>
+            </div>
+          </Fragment>
+        )}
       </div>
     </div>
   )
